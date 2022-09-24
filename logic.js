0a87cfc4c4426e18f0372fe5c12a6f2149941d23
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2022-09-24 13:38:16 +0100

    add js

---
+++ logic.js
@@ -0,0 +1,19 @@
+var fruits, emojis;
+
+fruits = ["Watermelon", "Melon", "Grapes"];
+emojis = ["🍉", "🍈", "🍇"];
+let element_list = document.getElementById("list");
+while (!!fruits.length) {
+  if (--window.LoopTrap <= 0) throw "Infinite loop.";
+  let new_li = document.createElement("li");
+  let new_span = document.createElement("span");
+  new_span.innerText = fruits.shift();
+
+  new_li.appendChild(new_span);
+  let new_span2 = document.createElement("span");
+  new_span2.innerText = emojis.shift();
+
+  new_li.appendChild(new_span2);
+
+  element_list.appendChild(new_li);
+}
